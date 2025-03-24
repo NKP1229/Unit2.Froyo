387f9517b7753a6db7bff41c7506c41a3ce733c9
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-03-24 14:18:17 -0400

    updated css and p tag user results

--- Froyo.js
+++ Froyo.js
@@ -12,7 +12,13 @@ function FindDuplicates(input) {
   }
   console.log(Flavors);
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const results = document.getElementById("Display");
+  for (const flavor in Flavors) {
+    results.textContent = `${results.textContent}  ${flavor}: ${Flavors[flavor]}.`;
+  }
+});
 
 const Prompt = "Please enter a comma seperated list of froyo flavors:";
-let userInput = prompt(Prompt);
+let userInput = prompt(Prompt, "chocolate, coffee, coffee, vanilla, chocolate");
 FindDuplicates(userInput);
